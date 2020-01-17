7b0f0ed2bd5b040f97a84f5c9b5fd0a7148fdc2e
Johanna Santos <johannasabsantos@gmail.com>
2020-01-16 22:54:21 -0300

    prueba example tutorial

---
+++ pruebaCuentas.js
@@ -0,0 +1,11 @@
+describe('Cuentas', function () {
+    var a = 1 + 1;
+    var b = 2;
+    it('vamos a ver que sume bien', function () {
+        expect(a == b).to.equal(true);
+    });
+    it('vamos a ver que reste bien', function () {
+        expect(a == b).to.equal(0);
+    }
+    )
+})
